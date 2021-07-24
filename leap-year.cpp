cc12d8ad0aeebb79ad82ccea961c62bc83b18de7
BengalDroid <bengaldroid@gmail.com>
2021-07-24 10:52:21 +0600

    leap year

---
+++ leap-year.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int year;
+    cout<<"Enter a year: ";
+    cin>>year;
+    if(year%4==0){
+        cout<<year<<"is a leap year";
+    }
+    else{
+        cout<<year<<"is not a leap year";
+    }
+    if(year%100==0){
+        cout<<year<<"is a leap year";
+    }
+    else{
+        cout<<year<<"is not a leap year";
+    }
+    if(year%400){
+        cout<<year<<"is a leap year";
+    }
+    else{
+        cout<<year<<"is not a leap year";
+    }
+    return 0;
+}
