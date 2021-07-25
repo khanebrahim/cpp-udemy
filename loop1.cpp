04da3f2727cde366dbbaa2222505864de04520df
BengalDroid <bengaldroid@gmail.com>
2021-07-25 13:41:29 +0600

    sum

--- loop1.cpp
+++ loop1.cpp
@@ -1,15 +1,16 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int r ,n;
+    int r ,n,sum=0;
     cout<<"Enter a number: ";
     cin>>n;
     while(n!=0)
 {
     r=n%10;
     n=n/10;
-    cout<<r<<endl;
+    // cout<<r<<endl;
+    sum=sum+r;
 }
-cout<<endl;
+cout<<sum<<endl;
 return 0;
 }
