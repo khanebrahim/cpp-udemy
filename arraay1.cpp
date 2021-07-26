ae15fa59b8d605261349ceb34cd107f6b1f87345
BengalDroid <bengaldroid@gmail.com>
2021-07-26 12:14:09 +0600

    cchar

---
+++ arraay1.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int A[6]={2,3,4,5,6,7};
+    for(int E:A){
+        cout<<E<<endl;
+    }
+    return 0;
+}
---
+++ array2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char A[6]={'A',66,67,75,76,'z'};
+    for(auto E:A){
+        cout<<E<<endl;
+    }
+    return 0;
+}
