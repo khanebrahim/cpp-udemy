7f34bf5d663b923930980d28293bb48d02b2089a
BengalDroid <bengaldroid@gmail.com>
2021-07-30 23:45:26 +0600

    value change

--- nested1-loop.cpp
+++ nested1-loop.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     for(int i=1; i<=5; i++){
-        for(int j=1;j<=5; j++){
+        for(int j=1;j<=6; j++){
             cout<<"("<<i<<","<<j<<")";
         }
         cout<<endl;
