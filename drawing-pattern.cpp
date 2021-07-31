f7757965591770fac12c9a1851ce6238841d0ea9
BengalDroid <bengaldroid@gmail.com>
2021-07-31 12:05:06 +0600

    same work

--- drawing-pattern.cpp
+++ drawing-pattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main (){
     for(int i=1;i<=5;i++){
-        for(int j=1;j<=5-i;j++){
+        for(int j=1;j<=i;j++){
             cout<<"* ";
         }
         cout<<endl;
