cba17baf1ddea9768a3b3a207f833a4b51c3bc79
BengalDroid <bengaldroid@gmail.com>
2021-07-31 12:04:42 +0600

    change logic

--- drawing-pattern.cpp
+++ drawing-pattern.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main (){
     for(int i=1;i<=5;i++){
-        for(int j=1;j<=5-i+1;j++){
+        for(int j=1;j<=5-i;j++){
             cout<<"* ";
         }
         cout<<endl;
