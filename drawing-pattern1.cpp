189aca8d1f172684b6f05e89180914a4057ef753
BengalDroid <bengaldroid@gmail.com>
2021-07-31 13:24:08 +0600

    pyramid

--- drawing-pattern1.cpp
+++ drawing-pattern1.cpp
@@ -4,7 +4,7 @@ int main (){
     for(int i=1; i<=7; i++){
         for(int j=1; j<=7; j++){
             if(i+j>7){
-                cout<<"* ";
+                cout<<" *  ";
             }
             else{
                 cout<<"  ";
