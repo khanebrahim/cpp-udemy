b519509ae1946ca6e449b2b21c9878a02f2dd176
BengalDroid <bengaldroid@gmail.com>
2021-07-31 13:16:21 +0600

    drawing awesome

--- drawing-pattern1.cpp
+++ drawing-pattern1.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 using namespace std;
 int main (){
-    for(int i=1; i<=5; i++){
-        for(int j=1; j<=5; j++){
-            if(i+j>5){
+    for(int i=1; i<=7; i++){
+        for(int j=1; j<=7; j++){
+            if(i+j>7){
                 cout<<"* ";
             }
             else{
